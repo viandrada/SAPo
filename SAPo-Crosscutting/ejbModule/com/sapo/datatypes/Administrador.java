d376a7d5ea3c631c3c209cc752625117a435c08b
viandrada <victoria.andrada@gmail.com>
2015-10-04 19:48:30 -0200

    -Agrego datatype Administrador

---
+++ SAPo-Crosscutting/ejbModule/com/sapo/datatypes/Administrador.java
@@ -0,0 +1,45 @@
+package com.sapo.datatypes;
+
+import java.util.Date;
+
+public class Administrador {
+	
+	private int idAdministrador;
+	private String nombre;
+	private String email;
+	private String password;
+	private Date fecha;
+	
+	public int getIdAdministrador() {
+		return idAdministrador;
+	}
+	public void setIdAdministrador(int idAdministrador) {
+		this.idAdministrador = idAdministrador;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+}
